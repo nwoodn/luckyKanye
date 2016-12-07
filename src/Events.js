ecea41db0a96bd02f122b1e1ffb8fbe3ba598ac4
sarahfeldmann <sarahf95@gmail.com>
2016-12-06 22:13:14 -0800

    Add Kanye's face

--- src/Events.js
+++ src/Events.js
@@ -2,16 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import eventsJson from './events.json';
 import { Form, FormControl, InputGroup, Button, Glyphicon } from 'react-bootstrap';
-// import { Map, Marker, Popup, TileLayer} from 'react-leaflet';
-
-
 import L from 'leaflet';
-// import controller from './EventController';
-
-// api key = Pd4kqfGVGCbGnj2f
-// Jam Base api key = t5wx9dw3ce7vbsyyjz7qqhrx
-
-//
 
 
 var SAMPLE_EVENTS = [
@@ -22,10 +13,8 @@ var SAMPLE_EVENTS = [
 class Events extends React.Component {
 	constructor(props) {
 		super(props);
-
 		this.state = { events: eventsJson['concerts'] };
 		this.setState = this.setState.bind(this);
-		// this.fetchData = this.fetchData.bind(this);
 	}
 
 	render() {
@@ -34,11 +23,10 @@ class Events extends React.Component {
 			<div className="Events">
 				<div className="Event-header">
 					<h2>Events</h2>
-					<MyMap marker={this.state.events}/>
+					<MyMap marker={this.state.events} />
 				</div>
 				<main>
 					<EventTable events={this.state.events} />
-
 				</main>
 			</div>
 		);
@@ -47,11 +35,9 @@ class Events extends React.Component {
 
 class EventTable extends React.Component {
 	render() {
-
 		var rows = this.props.events.map(function (eventObj) {
 			return <EventRow event={eventObj} />;
 		});
-
 		return (
 			<div>
 				{rows}
@@ -66,7 +52,6 @@ class EventRow extends React.Component {
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
 	}
-
 	handleClick() {
 		console.log("You clicked on", this.props.event.city);
 	}
@@ -82,77 +67,6 @@ class EventRow extends React.Component {
 		);
 	}
 }
-
-
-// class SearchEvents extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = { clickCount: 0, searchValue: '' };
-// 		this.handleClick = this.handleClick.bind(this);
-// 		this.handleChange = this.handleChange.bind(this);
-// 	}
-// 	handleClick(event) {
-// 		console.log("you clicked me");
-// 		this.props.searchCallBack(this.state.searchValue);
-// 	}
-// 	handleChange(event) {
-// 		var newValue = event.target.value;
-// 		console.log("user has typed", newValue);
-// 		this.setState({ stateSearchValue: newValue })
-// 	}
-// 	searchCallBack(){
-// 		fetch('events.json')
-// 		.then(function(response){
-// 			return response.json();
-// 		})
-// 		.then(calculate())
-// 	}
-
-
-// 	calculate(eventsArray){
-// 		var searchedEventsArray = eventsArray.filter(function(eventObj){
-// 			return (eventObj.city === this.state.searchValue)
-// 		});
-
-// 		console.log(searchedEventsArray);
-
-// 		var totalNum = searchedEventsArray.lengeth;
-// 		console.log("total num: ", totalNum);
-// 	}
-
-// 	render() {
-// 			// main();
-// 		return (
-// 			<Form inline>
-// 				<InputGroup>
-// 					<InputGroup.Button>
-// 						<Button onClick={this.handleClick} className="eventSearch">
-// 							<Glyphicon glyph="search" aria-label="Search" />
-// 						</Button>
-// 					</InputGroup.Button>
-// 					<FormControl type="text" placeholder="CITY"  onChange={this.handleChange} />
-// 					<InputGroup.Addon>{this.props.totalResults}results </InputGroup.Addon>
-// 				</InputGroup>
-// 			</Form>
-// 		);
-// 	}
-// }
-
-// class Map extends React.Component {
-
-
-// 	// 			// id: 'sarahf95.2a1n7d6j',
-// 	// accessToken: 'pk.eyJ1Ijoic2FyYWhmOTUiLCJhIjoiY2l3YndsZTB5MDRvOTJ0bGtvazZvdGZsYiJ9.gBaTfDLpBwuLDxMfsBBxjw'
-// 	//	}).addTo(mymap);
-// 	const position = [51.505, -0.09];
-// 	const map = (
-// 		<Map center={position} zoom={13}>
-// 			<TileLayer url='https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}'
-// 				attribution='Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors' />
-// 		</Map>
-// 	);
-// 	render(map, document.getElementById('mapid'));
-// }
 
 class MyMap extends Component {
 	constructor() {
@@ -170,16 +84,20 @@ class MyMap extends Component {
 				layers: [L.tileLayer("https://api.mapbox.com/styles/v1/sarahf95/ciwe83x0y001u2qqowgkeych8/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FyYWhmOTUiLCJhIjoiY2l3YndsZTB5MDRvOTJ0bGtvazZvdGZsYiJ9.gBaTfDLpBwuLDxMfsBBxjw", { attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>' })],
 				attributionControl: false,
 			});
-			map.fitWorld();
-			// var marker = this.props.events.map(function (markerObj) {
+			map.setView(new L.LatLng(37.0902, -95.7129), 4);
 
-			// 	return <Markers markers={markerObj} />;
-			var markerArray = this.props.marker;
-			markerArray.forEach(function(marker){
-				L.marker([marker.lat, marker.long]).addTo(map);
+			var kanyeIcon = L.icon({
+				iconUrl: 'kanye.png',
+				iconSize: [21, 32], // size of the icon
+				popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
 			});
-			//var marker = 
-			// });
+
+
+			var markerArray = this.props.marker;
+			markerArray.forEach(function (marker) {
+				L.marker([marker.lat, marker.long], {icon: kanyeIcon}).addTo(map).bindPopup("VENUE: " + marker.venue + ", " + marker.date);
+			});
+
 			return this.setState({
 				map: map
 			});
@@ -189,35 +107,9 @@ class MyMap extends Component {
 	render() {
 
 		return <div id="map" style={{ height: 500 }}>
-	
+
 		</div>;
 	}
 }
-
-// class Markers extends React.Component {
-// 	render() {
-// 		console.log(this);
-
-// 		return (
-// 			L.marker([event['lat'], event['long']]).addTo(map)
-// 		);
-// 	}
-//}
-
-// class Marker extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-
-// 	}
-
-// 	render() {
-// 		var MyMap = this.state.map;
-// 		var event = this.props.event; //shortcut
-// 		var marker = L.marker([event.lat, event.long]).addTo(MyMap);
-// 		return (
-// 			{ marker }
-// 		);
-// 	}
-// }
 
 export default Events;
